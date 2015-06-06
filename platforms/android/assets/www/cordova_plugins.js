18a4f402bd083d479745a7e86f3b8f6aed5efde5
valentingeorgiev <valio.v.georgiev@gmail.com>
2015-06-06 17:36:35 +0300

    Design

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,18 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "id": "cordova-plugin-device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
         "id": "cordova-plugin-camera.Camera",
         "clobbers": [
@@ -39,16 +27,28 @@ module.exports = [
         "clobbers": [
             "CameraPopoverHandle"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0",
-    "cordova-plugin-device": "1.0.0",
+    "cordova-plugin-camera": "1.1.0",
     "cordova-plugin-console": "1.0.0",
+    "cordova-plugin-device": "1.0.0",
     "cordova-plugin-geolocation": "1.0.0",
-    "cordova-plugin-camera": "1.1.0"
+    "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,51 +1,51 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+    this.$el.on('click', '.change-pic-btn', this.changePicture);
+    this.$el.on('click', '.add-location-btn', this.addLocation);
+    
 };
 
+
 app.initialize();
+
+
+this.changePicture = function(event) {
+  event.preventDefault();
+  if (!navigator.camera) {
+      alert("Camera API not supported", "Error");
+      return;
+  }
+  var options =   {   quality: 50,
+                      destinationType: Camera.DestinationType.DATA_URL,
+                      sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
+                      encodingType: 0     // 0=JPG 1=PNG
+                  };
+
+  navigator.camera.getPicture(
+      function(imgData) {
+          $('.media-object', this.$el).attr('src', "data:image/jpeg;base64,"+imgData);
+      },
+      function() {
+          alert('Error taking picture', 'Error');
+      },
+      options);
+
+  return false;
+};
+
+this.addLocation = function(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(
+      function(position) {
+          alert(position.coords.latitude + ',' + position.coords.longitude);
+      },
+      function() {
+          alert('Error getting location');
+      });
+  return false;
+};
+
+
